0bfad317a064099fb842bc542a2bb44248319e17
Leon Tappe <ltappe@mail.uni-paderborn.de>
2019-01-20 08:53:42 +0100

    make joboptions work again hopefully

--- lib/widgets/joboption_switches.dart
+++ lib/widgets/joboption_switches.dart
@@ -1,6 +1,7 @@
 import 'package:blocs_copyclient/joblist.dart';
 import 'package:blocs_copyclient/src/models/joboptions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 ///
 /// Switches for setting options on print jobs using BLoCs
@@ -23,100 +24,153 @@ class _JoboptionSwitchesState extends State<JoboptionSwitches> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        (job.jobInfo.pagecount * job.jobOptions.copies < 2)
-            ? null
-            : Column(children: <Widget>[
-                ListTile(
-                  onTap: null,
-                  title: Text('Duplex'),
-                  trailing: DropdownButton(
-                    items: <Map<int, String>>[
-                      {0: 'Aus'},
-                      {1: 'Lange Kante'},
-                      {2: 'Kurze Kante'},
-                    ]
-                        .map((Map<int, String> duplex) => DropdownMenuItem<int>(
-                              value: duplex.keys.single,
-                              child: Text(duplex.values.single),
-                            ))
-                        .toList(),
-                    value: job.jobOptions.duplex,
-                    onChanged: (val) => null,
-                  ),
-                ),
-                Divider(indent: 10.0),
-              ]),
-        ListTile(
-          onTap: () => null,
-          leading: Icon(Icons.photo_size_select_large),
-          title: Text('A3'),
-          trailing: Switch(
-            onChanged: (val) => null,
-            value: job.jobOptions.a3,
-          ),
-        ),
-        Divider(indent: 10.0),
-        ListTile(
-          onTap: null,
-          leading: Icon(Icons.clear_all),
-          title: Text('Anzahl Kopien'),
-          trailing: Container(
-            width: 100.0,
-            child: Row(
-              children: <Widget>[
-                (job.jobOptions.copies > 1)
-                    ? Expanded(
-                        flex: 1,
-                        child: IconButton(
-                          icon: Icon(Icons.expand_more),
-                          onPressed: () => (job.jobOptions.copies > 1)
-                              ? null
-                              : Scaffold.of(context).showSnackBar(SnackBar(
-                                  content: Text(
-                                      'Nicht weniger als eine Kopie möglich'),
-                                )),
+    final JoblistBloc joblistBloc = BlocProvider.of<JoblistBloc>(context);
+    return BlocBuilder<JoblistEvent, JoblistState>(
+      bloc: joblistBloc,
+      builder: (BuildContext context, JoblistState state) {
+        return Column(
+          children: <Widget>[
+            (job.jobInfo.colored > 0) ? ListTile(
+              onTap: () {
+                job.jobOptions.color = !job.jobOptions.color;
+                joblistBloc.onUpdateOptionsById(job.id, job.jobOptions);
+              },
+              leading: Icon(Icons.palette),
+              title: Text('Farbe'),
+              trailing: Switch(
+                onChanged: (val) {
+                  job.jobOptions.color = val;
+                  joblistBloc.onUpdateOptionsById(job.id, job.jobOptions);
+                },
+                value: job.jobOptions.color,
+              ),
+            ) : null,
+            (job.jobInfo.pagecount * job.jobOptions.copies < 2)
+                ? null
+                : Column(
+                    children: <Widget>[
+                      ListTile(
+                        onTap: null,
+                        title: Text('Duplex'),
+                        trailing: DropdownButton(
+                          items: <Map<int, String>>[
+                            {0: 'Aus'},
+                            {1: 'Lange Kante'},
+                            {2: 'Kurze Kante'},
+                          ]
+                              .map((Map<int, String> duplex) =>
+                                  DropdownMenuItem<int>(
+                                    value: duplex.keys.single,
+                                    child: Text(duplex.values.single),
+                                  ))
+                              .toList(),
+                          value: job.jobOptions.duplex,
+                          onChanged: (val) {
+                            job.jobOptions.duplex = val;
+                            joblistBloc.onUpdateOptionsById(
+                                job.id, job.jobOptions);
+                          },
                         ),
-                      )
-                    : Expanded(
-                        flex: 1,
-                        child: Text(''),
                       ),
-                Expanded(
-                  flex: 1,
-                  child: Text(
-                    job.jobOptions.copies.toString(),
-                    textAlign: TextAlign.center,
+                      Divider(indent: 10.0),
+                    ],
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: IconButton(
-                    icon: Icon(Icons.expand_less),
-                    onPressed: () => (job.jobOptions.copies < 1000)
-                        ? null
-                        : Scaffold.of(context).showSnackBar(SnackBar(
-                            content: Text('Nicht mehr als 999 Kopien möglich'),
-                          )),
-                  ),
-                ),
-              ].where((widget) => widget != null).toList(),
+            ListTile(
+              onTap: () {
+                job.jobOptions.a3 = !job.jobOptions.a3;
+                joblistBloc.onUpdateOptionsById(job.id, job.jobOptions);
+              },
+              leading: Icon(Icons.photo_size_select_large),
+              title: Text('A3'),
+              trailing: Switch(
+                onChanged: (val) {
+                  job.jobOptions.a3 = val;
+                  joblistBloc.onUpdateOptionsById(job.id, job.jobOptions);
+                },
+                value: job.jobOptions.a3,
+              ),
             ),
-          ),
-        ),
-        (job.jobOptions.copies > 1)
-            ? ListTile(
-                onTap: () => null,
-                leading: null,
-                title: Text('Gleiche Seiten zusammenstellen'),
-                trailing: Switch(
-                  onChanged: (val) => null,
-                  value: job.jobOptions.collate,
+            Divider(indent: 10.0),
+            ListTile(
+              onTap: null,
+              leading: Icon(Icons.clear_all),
+              title: Text('Anzahl Kopien'),
+              trailing: Container(
+                width: 100.0,
+                child: Row(
+                  children: <Widget>[
+                    (job.jobOptions.copies > 1)
+                        ? Expanded(
+                            flex: 1,
+                            child: IconButton(
+                              icon: Icon(Icons.expand_more),
+                              onPressed: () {
+                                if (job.jobOptions.copies > 1) {
+                                  job.jobOptions.copies--;
+                                  joblistBloc.onUpdateOptionsById(
+                                      job.id, job.jobOptions);
+                                } else
+                                  Scaffold.of(context).showSnackBar(
+                                    SnackBar(
+                                      content: Text(
+                                          'Nicht weniger als eine Kopie möglich'),
+                                    ),
+                                  );
+                              },
+                            ),
+                          )
+                        : Expanded(
+                            flex: 1,
+                            child: Text(''),
+                          ),
+                    Expanded(
+                      flex: 1,
+                      child: Text(
+                        job.jobOptions.copies.toString(),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: IconButton(
+                        icon: Icon(Icons.expand_less),
+                        onPressed: () {
+                          if (job.jobOptions.copies < 1000) {
+                            job.jobOptions.copies++;
+                            joblistBloc.onUpdateOptionsById(
+                                job.id, job.jobOptions);
+                          } else
+                            Scaffold.of(context).showSnackBar(
+                              SnackBar(
+                                content:
+                                    Text('Nicht mehr als 999 Kopien möglich'),
+                              ),
+                            );
+                        },
+                      ),
+                    ),
+                  ].where((widget) => widget != null).toList(),
                 ),
-              )
-            : null,
-        /* 
+              ),
+            ),
+            (job.jobOptions.copies > 1)
+                ? ListTile(
+                    onTap: () {
+                      job.jobOptions.collate = !job.jobOptions.collate;
+                      joblistBloc.onUpdateOptionsById(job.id, job.jobOptions);
+                    },
+                    leading: null,
+                    title: Text('Gleiche Seiten zusammenstellen'),
+                    trailing: Switch(
+                      onChanged: (val) {
+                        job.jobOptions.collate = val;
+                        joblistBloc.onUpdateOptionsById(job.id, job.jobOptions);
+                      },
+                      value: job.jobOptions.collate,
+                    ),
+                  )
+                : null,
+            /* 
           TODO: Preparation for printrange
           
            ListTile( 
@@ -124,49 +178,61 @@ class _JoboptionSwitchesState extends State<JoboptionSwitches> {
             title:  Text('Zu druckende Seiten'),
             trailing:  Text(job.jobOptions.range),
           ),*/
-        Divider(indent: 10.0),
-        ((job.jobInfo.pagecount /* * job.jobOptions.copies*/) > 1)
-            ? ListTile(
-                onTap: null,
-                title: Text('Seiten pro Blatt'),
-                trailing: DropdownButton(
-                  items:
-                      (((job.jobInfo.pagecount /* * job.jobOptions.copies*/) >
+            Divider(indent: 10.0),
+            ((job.jobInfo.pagecount * job.jobOptions.copies) > 1)
+                ? ListTile(
+                    onTap: null,
+                    title: Text('Seiten pro Blatt'),
+                    trailing: DropdownButton(
+                      items: (((job.jobInfo
+                                      .pagecount /* * job.jobOptions.copies*/) >
                                   2)
                               ? [1, 2, 4]
                               : [1, 2])
-                          .map((int value) => DropdownMenuItem<int>(
-                                value: value,
-                                child: Text(value.toString()),
-                              ))
+                          .map(
+                            (int value) => DropdownMenuItem<int>(
+                                  value: value,
+                                  child: Text(value.toString()),
+                                ),
+                          )
                           .toList(),
-                  value: job.jobOptions.nup,
-                  onChanged: (val) => null,
-                ),
-              )
-            : null,
-        (job.jobOptions.nup > 2)
-            ? ListTile(
-                onTap: null,
-                title: Text('Reihenfolge auf Blatt'),
-                trailing: DropdownButton(
-                  items: <Map<int, String>>[
-                    _translateNupOrder(NupPageOrder.RIGHTTHENDOWN),
-                    _translateNupOrder(NupPageOrder.DOWNTHENRIGHT),
-                    _translateNupOrder(NupPageOrder.LEFTTHENDOWN),
-                    _translateNupOrder(NupPageOrder.DOWNTHENLEFT),
-                  ]
-                      .map((Map<int, String> order) => DropdownMenuItem<int>(
-                            value: order.keys.single,
-                            child: Text(order.values.single),
-                          ))
-                      .toList(),
-                  value: job.jobOptions.nupPageOrder,
-                  onChanged: (val) => null,
-                ),
-              )
-            : null,
-      ].where((widget) => widget != null).toList(),
+                      value: job.jobOptions.nup,
+                      onChanged: (val) {
+                        job.jobOptions.nup = val;
+                        joblistBloc.onUpdateOptionsById(job.id, job.jobOptions);
+                      },
+                    ),
+                  )
+                : null,
+            (job.jobOptions.nup > 2)
+                ? ListTile(
+                    onTap: null,
+                    title: Text('Reihenfolge auf Blatt'),
+                    trailing: DropdownButton(
+                      items: <Map<int, String>>[
+                        _translateNupOrder(NupPageOrder.RIGHTTHENDOWN),
+                        _translateNupOrder(NupPageOrder.DOWNTHENRIGHT),
+                        _translateNupOrder(NupPageOrder.LEFTTHENDOWN),
+                        _translateNupOrder(NupPageOrder.DOWNTHENLEFT),
+                      ]
+                          .map(
+                            (Map<int, String> order) => DropdownMenuItem<int>(
+                                  value: order.keys.single,
+                                  child: Text(order.values.single),
+                                ),
+                          )
+                          .toList(),
+                      value: job.jobOptions.nupPageOrder,
+                      onChanged: (val) {
+                        job.jobOptions.nupPageOrder = val;
+                        joblistBloc.onUpdateOptionsById(job.id, job.jobOptions);
+                      },
+                    ),
+                  )
+                : null,
+          ].where((widget) => widget != null).toList(),
+        );
+      },
     );
   }
 
