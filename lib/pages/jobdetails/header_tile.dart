6ff639c43bed950187f2c1408167a08f047ccdc0
Leon Tappe <ltappe@mail.uni-paderborn.de>
2019-03-31 05:02:10 +0200

    indicator fow pdf download state added on details

--- lib/pages/jobdetails/header_tile.dart
+++ lib/pages/jobdetails/header_tile.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:blocs_copyclient/joblist.dart';
+import 'package:blocs_copyclient/pdf_download.dart';
 import 'package:blocs_copyclient/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -30,8 +33,7 @@ class _HeaderTileState extends State<HeaderTile> {
     return BlocBuilder<JoblistEvent, JoblistState>(
       bloc: BlocProvider.of<JoblistBloc>(context),
       builder: (BuildContext context, JoblistState state) {
-        if (state.isResult)
-          _job = state.value.singleWhere((Job job) => job.id == _job.id);
+        if (state.isResult) _job = state.value.singleWhere((Job job) => job.id == _job.id);
         return Column(
           children: <Widget>[
             ListTile(
@@ -41,10 +43,37 @@ class _HeaderTileState extends State<HeaderTile> {
                 maxLines: 3,
                 overflow: TextOverflow.ellipsis,
               ),
-              subtitle: Text(
-                  DateTime.fromMillisecondsSinceEpoch(_job.timestamp * 1000)
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(DateTime.fromMillisecondsSinceEpoch(_job.timestamp * 1000)
                       .toString()
                       .split('.')[0]),
+                  (!Platform.isIOS)
+                      ? BlocBuilder<PdfEvent, PdfState>(
+                          bloc: BlocProvider.of<PdfBloc>(context),
+                          builder: (BuildContext context, PdfState state) {
+                            if (state.isResult || state.isInit) {
+                              print((state.value
+                                  .contains((PdfFile file) => file.id == _job.id)
+                                  .toString()));
+                              Iterable idResults = state.value.where((PdfFile file) => file.id == _job.id);
+                              return Text(
+                                (idResults.length == 1)
+                                    ? 'Heruntergeladen'
+                                    : 'Nicht heruntergeladen',
+                              );
+                            } else if (state.isBusy) {
+                              return Text('Am Herunterladen...');
+                            } else if (state.isException) {
+                              return Text(
+                                  'Fehler  beim Download der PDF: ${state.error.toString()}');
+                            }
+                          },
+                        )
+                      : Container(width: 0.0, height: 0.0),
+                ],
+              ),
               trailing: HeartPin(_job.id),
             ),
             ListTile(
@@ -55,8 +84,7 @@ class _HeaderTileState extends State<HeaderTile> {
                     textColor: Colors.grey[100],
                     label: Padding(
                       padding: EdgeInsets.only(right: 16.0),
-                      child: Text(
-                          '${((_job.priceEstimation ?? 0) / 100.0).toStringAsFixed(2)} €'),
+                      child: Text('${((_job.priceEstimation ?? 0) / 100.0).toStringAsFixed(2)} €'),
                     ),
                     icon: Padding(
                       padding: EdgeInsets.only(left: 16.0),
@@ -67,14 +95,13 @@ class _HeaderTileState extends State<HeaderTile> {
                       try {
                         target = await BarcodeScanner.scan();
                         if (target != null) {
-                          BlocProvider.of<JoblistBloc>(context)
-                              .onPrintById(target, _job.id);
+                          BlocProvider.of<JoblistBloc>(context).onPrintById(target, _job.id);
                           Navigator.of(context).pop();
                         }
                       } catch (e) {
                         print('MetaTile: $e');
-                        Scaffold.of(context).showSnackBar(SnackBar(
-                            content: Text('Es wurde kein Drucker ausgewählt')));
+                        Scaffold.of(context).showSnackBar(
+                            SnackBar(content: Text('Es wurde kein Drucker ausgewählt')));
                       }
                     },
                   ),
@@ -83,9 +110,7 @@ class _HeaderTileState extends State<HeaderTile> {
                     builder: (BuildContext context, UserState state) {
                       if (state.isResult) {
                         return Text(
-                          ((userBloc.user.credit -
-                                      (_job.priceEstimation / 100.0)) >
-                                  0)
+                          ((userBloc.user.credit - (_job.priceEstimation / 100.0)) > 0)
                               ? 'Neues Guthaben vmtl.: ${((userBloc.user.credit - _job.priceEstimation) / 100.0).toStringAsFixed(2)} €'
                               : 'Fehlendes Guthaben: ${(((userBloc.user.credit - _job.priceEstimation) / 100.0) * -1).toStringAsFixed(2)} €',
                           textAlign: TextAlign.left,
