b959a08e85495c6f610410071acd22b32dbecf79
Leon Tappe <ltappe@mail.uni-paderborn.de>
2019-10-12 16:19:03 +0200

    start adding web support

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ltappe/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ltappe/projects/copyclient_rx"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/ltappe/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.1.1"
+export "FLUTTER_BUILD_NUMBER=69"
